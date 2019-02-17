6ae7a2b1b7d9366805999ef324171cc231fa2bea
1895730 <1895730@E21033.CEGEPGIM.LCL>
2019-02-17 11:18:01 -0500

    Commiting order class by pavani

---
+++ OShoppingProject/src/oshoppingproject/Order.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oshoppingproject;
+
+import java.util.Date;
+
+/**
+ *
+ * @author 1895730
+ */
+public class Order {
+    Date date;
+    String paymethod;
+    float amount;
+    
+    
+}
